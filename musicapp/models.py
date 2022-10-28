08de2902208d2130eb9307a666f0396710e2ddf6
Cipher Phantom <oyepitan10@gmail.com>
2022-10-28 19:52:07 +0100

    Modify the models
    Set the default values and migrate musicapp to ensure no errors

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,24 +1,23 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Artiste(models.Model):
 
-    first_name = models.CharField()
-    last_name = models.CharField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     age = models.IntegerField()
 
 
 class Song(models.Model):
 
-    Artiste = models.ForeignKey(Artiste, on_delete=models.PROTECT)
-    title = models.CharField()
-    date_released = models.DateField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    date_released = models.DateField(default=datetime.today)
     likes = models.IntegerField()
-    artiste_id = models.IntegerField()
 
 
 class Lyric(models.Model):
 
-    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    content = models.CharField()
-    song_id = models.IntegerField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)

dd563138344472da3845250b87b4275756ee22ed
Cipher Phantom <oyepitan10@gmail.com>
2022-10-28 18:44:19 +0100

    Creating Models

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,9 @@
+from atexit import register
+from re import A
 from django.contrib import admin
-
+from .models import Song, Artiste, Lyric
 # Register your models here.
+
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+
+    first_name = models.CharField()
+    last_name = models.CharField()
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+
+    Artiste = models.ForeignKey(Artiste, on_delete=models.PROTECT)
+    title = models.CharField()
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.IntegerField()
+
+
+class Lyric(models.Model):
+
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField()
+    song_id = models.IntegerField()

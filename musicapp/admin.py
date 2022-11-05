036e4456ee98fa5c347ef2c1927d7e396ba56020
Cipher Phantom <oyepitan10@gmail.com>
2022-11-05 10:15:31 +0100

    Alterations
    I made modifications to the model fields and add more info to the admin views

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,9 +1,15 @@
-from atexit import register
-from re import A
 from django.contrib import admin
 from .models import Song, Artiste, Lyric
 # Register your models here.
 
-admin.site.register(Artiste)
-admin.site.register(Song)
-admin.site.register(Lyric)
+@admin.register(Artiste)
+class ArtisteAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'age']
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ['title', 'artiste_id', 'date_released']
+
+@admin.register(Lyric)
+class LyricAdmin(admin.ModelAdmin):
+    list_display = ['song_id']
--- musicapp/models.py
+++ musicapp/models.py
@@ -8,16 +8,25 @@ class Artiste(models.Model):
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.first_name
+
 
 class Song(models.Model):
 
     artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    date_released = models.DateField(default=datetime.today)
+    date_released = models.DateField(auto_now_add=True)
     likes = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} by {self.artiste_id.__str__()}"
 
 
 class Lyric(models.Model):
 
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
-    content = models.CharField(max_length=1000)
+    content = models.TextField()
+
+    def __str__(self):
+        return "Lyrics to {}".format(self.song_id.__str__())
